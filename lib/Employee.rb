3ff90d78535be604e4a5dbfa1c4b5f8c4b08ad71
github username <github email address>
2019-05-02 11:13:12 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,33 @@
 class Employee
+  attr_accessor :name, :salary, :manager_name
+ @@all = []
 
+  def initialize(name, salary, manager_name)
+    @name = name
+    @salary = salary
+    @manager_name = manager_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def manager
+    Manager.all.find{|boss| boss.name == manager_name}
+  end
+
+  def self.paid_over(salary)
+    @@all.select{|employee| employee.salary > salary}
+  end
+
+  def self.find_by_department(department)
+    @@all.find {|employee| employee.manager.department == department}
+  end
+
+  def tax_bracket
+    @@all.select do|employee|
+       self.salary+1000 > employee.salary &&  employee.salary > self.salary-1000
+    end
+  end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
+  attr_accessor :name, :department, :employees, :age
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @employees = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def hire_employee (name, salary)
+    newbie = Employee.new(name, salary, self.name)
+    @employees << newbie
+    newbie
+  end
+
+  def self.all_departments
+    @@all.map {|boss| boss.department}
+  end
+
+  def self.average_age
+    ages = @@all.map{|boss| boss.age}
+    ages.sum / ages.length
+  end
 
 end
