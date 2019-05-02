dd54e95fbb4b68eea68d9821e3ba0c187242d181
Danny Tseng <github email address>
2019-05-02 11:11:45 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,43 @@
 class Employee
 
+  attr_accessor :name, :salary, :manager
+
+  @@all = []
+
+  def initialize(name, salary, manager)
+    @name = name
+    @salary = salary
+    @manager = manager
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def manager_name
+    self.manager.name
+  end
+
+  def self.paid_over(salary)
+    self.all.select do |employee|
+      employee.salary > salary
+    end
+  end
+
+  def self.find_by_department(department)
+    #match = nil
+    match = nil
+    Manager.all.each do |manager|
+      match = manager.employees.first if manager.department == department && !match
+    end
+    match
+  end
+
+  def tax_bracket
+    @@all.select do |employee|
+      self.salary + 1001 > employee.salary && self.salary - 1001 < employee.salary
+    end
+  end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,36 @@
 class Manager
+  attr_accessor :name, :department, :age, :employees
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @employees = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def hire_employee(name, salary)
+    self.employees << Employee.new(name, salary, self)
+  end
+
+  def self.all_departments
+    self.all.map do |manager|
+      manager.department
+    end
+  end
+
+  def self.average_age
+    total_age = 0.0;
+    self.all.each do |manager|
+      total_age += manager.age
+    end
+    total_age / self.all.length
+  end
 
 end
