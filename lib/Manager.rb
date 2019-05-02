9a095b050ceefe91e27df0ddd4cbdda3828a6211
github username <github email address>
2019-05-02 11:15:37 -0700

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
+    attr_accessor :name, :department, :age
 
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self.name)
+    end
+
+    def employees
+        Employee.all.select{|employee|
+            employee.manager_name == self.name
+        }
+    end
+
+    def self.all_departments
+        @@all.map{|manager|
+            manager.department
+        }
+    end
+
+    def self.average_age
+        ages = []
+       ages =  @@all.map{|manager|
+            manager.age
+        }
+        ages.sum / ages.count
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,18 @@ require 'pry'
 
 #Test your code here
 
+caleb = Manager.new('Caleb', 'Flatiron Students', 19)
+bob = Manager.new('bob', 'Flatiron Students', 25)
+dave = Manager.new('dave', 'Flatiron Students', 21)
+frank = Manager.new('frank', 'Flatiron Students', 35)
+# binding.pry
+# puts "done"
+puts caleb.name
+puts caleb.age
+puts caleb.department
+caleb.hire_employee('vadim', 10000)
+caleb.hire_employee('sean', 10900)
+caleb.hire_employee('danny', 22000)
 
 binding.pry
-puts "done"
+0
