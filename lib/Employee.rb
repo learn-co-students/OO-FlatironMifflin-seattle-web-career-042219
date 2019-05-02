fba8a4dacee649312a7ce38aedcd666e518c01b5
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-02 10:48:44 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,29 @@
 class Employee
+attr_accessor :name, :salary, :manager
+@@all = []
 
+def initialize(name, salary, manager)
+@name= name
+@salary= salary
+@manager = manager
+@@all<< self
+end
+
+def self.all
+@@all
+end
+
+def self.paid_over(num)
+@@all.select{|employee|employee.salary>num}
+end
+
+def self.find_by_department(department)
+Manager.all.map{|manager|manager.employees if manager.department==department}.compact.flatten.first
+end
+
+def tax_bracket
+max = self.salary + 1000
+min = self.salary - 1000
+ a = @@all.map{|employee|employee.name if employee.salary > min && employee.salary < max && employee.name != self.name}.compact
+end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
+attr_accessor :name, :age, :department
+@@all = []
 
+def initialize(name, age, department)
+@name= name
+@age= age
+@department = department
+@@all<< self
+end
+
+def self.average_age
+ages = @@all.map{|manager|manager.age}
+ages.sum.to_f/ages.length
+end
+
+
+def self.all_departments
+departments = @@all.map{|manager|manager.department}
+departments.uniq
+end
+
+def hire_employee (name, salary)
+Employee.new(name, salary, self)
+end
+
+def self.all
+@@all
+end
+
+def employees
+Employee.all.select{|employee| employee.manager ==self}
+end
 end
