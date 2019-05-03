767b069e877cabd598afcb6ef9bb21f9c44a8843
Milan <milan.dhar@gmail.com>
2019-05-03 09:38:42 -0700

    finsihed

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,44 @@
 class Manager
+  attr_accessor :name, :department, :age
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    Employee.all.select do |employee|
+      employee.manager_name == self.name
+    end
+  end
+
+  def hire_employee(name, salary)
+    Employee.new(name, salary, self.name)
+  end
+
+  def self.all_departments
+    Manager.all.map do |manager|
+      manager.department
+    end.uniq
+  end
+
+  def self.average_age
+    total_age = 0.0
+    Manager.all.each do |manager|
+      total_age += manager.age
+    end
+    total_age.to_f / Manager.all.count
+  end
+
+  
+
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,11 @@ require 'pry'
 
 #Test your code here
 
+  michael = Manager.new("Michael", "Sales", 45)
+  daryl = Manager.new("Daryl", "Warehouse", 40)
+  jim = Employee.new("Jim", 30000, "Michael")
+  roy = Employee.new("Roy", 25000, "Daryl")
+  dwight = Employee.new("Dwight", 30500, "Michael")
 
 binding.pry
 puts "done"
