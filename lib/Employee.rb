b21ab02ea660d5cb4f60d0184ff3adc058363748
github username <github email address>
2019-05-04 21:05:25 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,49 @@
+
 class Employee
+
+  @@all = []
+
+  attr_accessor :name, :salary, :manager_name
+
+  def initialize(name, salary, manager_name)
+    @name = name
+    @salary = salary
+    @manager_name = manager_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.paid_over(sal_limit)
+    over_ary = []
+    @@all.each do |emp_obj|
+      if emp_obj.salary > sal_limit
+        over_ary << emp_obj.name
+      end
+    end
+    over_ary
+  end
+
+  def self.find_by_department(department)
+    mgr = Manager.all.find do |mgr_obj|
+      mgr_obj.department == department
+    end
+    @@all.find do |emp_obj|
+      if emp_obj.manager_name == mgr
+        emp_obj
+      end
+    end
+  end
+
+  def tax_bracket
+    callers_salary = self.salary
+    @@all.select do |emp_obj|
+       emp_obj.salary <= callers_salary + 1000 &&
+       emp_obj.salary >= callers_salary - 1000 &&
+       emp_obj != self
+    end
+  end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,32 @@ require 'pry'
 
 #Test your code here
 
+#manager objects
+laura = Manager.new("Laura", "QA", 40)
+sarah = Manager.new("Sarah", "Operations", 35)
+kevin = Manager.new("Kevin", "Development", 45)
+
+
+#employee objects
+lisa = laura.hire_employee("Lisa", 10000)
+dave = laura.hire_employee("Dave", 11000)
+katie = sarah.hire_employee("Katie", 11300)
+erin = sarah.hire_employee("Erin", 9000)
+tyler = kevin.hire_employee("Tyler", 8000)
+sam = kevin.hire_employee("Sam", 8500)
+
+#other method tests Manager
+all_managers = Manager.all
+lauras_employees = laura.employees
+all_departments = Manager.all_departments
+average_age = Manager.average_age
+
+
+#other method tests Employee
+all_employees = Employee.all
+over_limit = Employee.paid_over(10000)
+by_dept = Employee.find_by_department("QA")
+by_bracket = tyler.tax_bracket
 
 binding.pry
 puts "done"
