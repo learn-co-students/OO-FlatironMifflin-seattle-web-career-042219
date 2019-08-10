b076558876de5f04e375f417cc1c59fd1e8d3ff4
github username <gregm9999@gmail.com>
2019-08-09 21:43:48 -0700

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+
+    @@all=[]
+
+    def initialize(name, department, age)
+        @name=name
+        @department=department
+        @age=age
+        @@all<<self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select do |employee|
+            employee.manager == self
+        end
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        self.all.map do |manager|
+            manager.department
+        end
+    end
+
+    def self.average_age
+        self.all.map do |manager|
+            manager.age
+        end.sum/self.all.size.to_f
+    end
+
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,15 @@ require 'pry'
 
 
 #Test your code here
+m1 = Manager.new("Adam", "Accounting", 11)
+m2 = Manager.new("Babs", "Bookkeeping", 22)
+m3 = Manager.new("Carl", "Computing", 33)
+
+e1 = Employee.new("Xavier", 10000, m1)
+e2 = Employee.new("Yanick", 20000, m2)
+e2 = Employee.new("Yves", 20001, m2)
+e3 = Employee.new("Zack", 30000, m3)
 
 
-binding.pry
+Pry.start
 puts "done"
